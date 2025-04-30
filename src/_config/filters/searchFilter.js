e2ef16d0aa75a39d01dd744c0bc280135121c468
Jens Andreasson <jensandreasson77@gmail.com>
2025-04-30 10:02:38 +0200

    Search (#85)
    * feat: implement search functionality with ElasticLunr and update navigation
    
    * feat: refactor navigation component and ensure search modal inclusion in base layout
    
    * feat: update navbar and site footer styles for improved layout and spacing
    
    * feat: preload Nunito font and set font-display to optional for improved performance

---
+++ src/_config/filters/searchFilter.js
@@ -0,0 +1,39 @@
+import { ca } from "date-fns/locale"
+import elasticlunr from "elasticlunr"
+
+const searchFilter = async (collection) => {
+    // what fields we'd like our index to consist of
+    const index = elasticlunr(function () {
+        this.addField("title")
+        this.addField("summary")
+        this.addField("tags")
+        this.addField("category")
+        this.setRef("id")
+    })
+
+    // loop through each page and add it to the index
+    for (const page of collection) {
+        const { data } = await page.template.read()
+
+        // if the page is excluded from collections, skip it
+        if (data.eleventyExcludeFromCollections) continue
+        // if the page is not a post, skip it
+        if (!page.url.includes("/posts/")) continue
+        // if the page is not a markdown file, skip it
+        if (!page.inputPath.endsWith(".md")) continue
+
+        const tags = data.tags ? data.tags.toString() : ""
+
+        index.addDoc({
+            id: page.url,
+            title: data.title,
+            summary: data.summary || "",
+            tags: tags,
+            category: data.category || "",
+        })
+    }
+
+    return index.toJSON()
+}
+
+export { searchFilter }
--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,69 +1,69 @@
 const siteSearch = () => {
-  const toggleSearch = () => {
-    const searchModal = document.querySelector("#search-modal")
-    searchModal.classList.toggle("hidden")
-    const searchInput = document.querySelector("#search-input")
-    searchInput.focus()
-  }
-
-  document
-    .querySelector("#toggleSearch")
-    .addEventListener("click", toggleSearch)
-
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "k" && e.ctrlKey) {
-      e.preventDefault()
-      toggleSearch()
+    const toggleSearch = () => {
+        const searchModal = document.querySelector("#search-modal")
+        searchModal.classList.toggle("hidden")
+        const searchInput = document.querySelector("#search-input")
+        searchInput.focus()
     }
-  })
 
-  const search = (e) => {
-    const results = window.searchIndex.search(e.target.value, {
-      bool: "OR",
-      expand: true,
+    document
+        .querySelector("#toggleSearch")
+        .addEventListener("click", toggleSearch)
+
+    window.addEventListener("keydown", (e) => {
+        if (e.key === "k" && e.ctrlKey) {
+            e.preventDefault()
+            toggleSearch()
+        }
     })
 
-    const resultElement = document.querySelector("#search-results")
+    const search = (e) => {
+        const results = window.searchIndex.search(e.target.value, {
+            bool: "OR",
+            expand: true,
+        })
 
-    let i = 0
+        const resultElement = document.querySelector("#search-results")
 
-    resultElement.innerHTML = ""
-    if (results && results.length > 0) {
-      resultElement.classList.remove("hidden")
-      results.map((result) => {
-        if (i > 10) return
-        const { id, title, summary } = result.doc
-        const li = document.createElement("li")
-        // li.classList.add("search-modal-results-item")
-        resultElement.appendChild(li)
+        let i = 0
 
-        const a = document.createElement("a")
-        a.setAttribute("href", id)
-        a.textContent = title
-        li.appendChild(a)
+        resultElement.innerHTML = ""
+        if (results && results.length > 0) {
+            resultElement.classList.remove("hidden")
+            results.map((result) => {
+                if (i > 10) return
+                const { id, title, summary } = result.doc
+                const li = document.createElement("li")
+                // li.classList.add("search-modal-results-item")
+                resultElement.appendChild(li)
 
-        const p = document.createElement("p")
-        p.textContent = summary || ""
-        li.appendChild(p)
+                const a = document.createElement("a")
+                a.setAttribute("href", id)
+                a.textContent = title
+                li.appendChild(a)
 
-        i += 1
-      })
-    } else if (results && results.length < 1) {
-      resultElement.classList.add("hidden")
-    } else {
-      resultElement.classList.remove("hidden")
-      resultElement.innerHTML =
-        '<li class="search-modal-results-item--disabled">Hittade inga sökresultat, försök igen</li>'
+                const p = document.createElement("p")
+                p.textContent = summary || ""
+                li.appendChild(p)
+
+                i += 1
+            })
+        } else if (results && results.length < 1) {
+            resultElement.classList.add("hidden")
+        } else {
+            resultElement.classList.remove("hidden")
+            resultElement.innerHTML =
+                '<li class="search-modal-results-item--disabled">Hittade inga sökresultat, försök igen</li>'
+        }
     }
-  }
 
-  fetch("/search-index.json").then((response) =>
-    response.json().then((rawIndex) => {
-      // eslint-disable-next-line no-undef
-      window.searchIndex = elasticlunr.Index.load(rawIndex)
-      document.querySelector("#search-input").addEventListener("input", search)
-    })
-  )
+    fetch("/search-index.json").then((response) =>
+        response.json().then((rawIndex) => {
+            // eslint-disable-next-line no-undef
+            window.searchIndex = elasticlunr.Index.load(rawIndex)
+            document.querySelector("#search-input").addEventListener("input", search)
+        })
+    )
 }
 
 export { siteSearch }
