94b2a13c2d70ecebe405fdb54c7d9dd600d7aa75
jensadev <jensandreasson77@gmail.com>
2025-04-30 11:50:08 +0200

    feat: add elasticlunr.js for client-side search and update asset paths

--- eleventy.config.js
+++ eleventy.config.js
@@ -167,7 +167,9 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/robots.txt")
     eleventyConfig.addPassthroughCopy("./src/favicon.ico")
     eleventyConfig.addPassthroughCopy("./src/_redirects")
-    eleventyConfig.addPassthroughCopy("./src/assets/")
+    eleventyConfig.addPassthroughCopy("./src/assets/fonts")
+    eleventyConfig.addPassthroughCopy("./src/assets/thumbs")
+    eleventyConfig.addPassthroughCopy("./src/assets/js/elasticlunr.min.js", "/assets/js/")
 
     return {
         templateFormats: ["njk", "md"],
